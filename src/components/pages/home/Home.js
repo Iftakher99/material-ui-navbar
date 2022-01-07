7e51c0d46f6a4b04524ae3d5febdf66fddc7523c
Iftakher99 <19tonmoy99@gmail.com>
2022-01-07 21:45:22 +0600

    initial code

---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header2 from "../../shared/header/Header2";
+
+const Home = () => {
+  return (
+    <div>
+      <Header2></Header2>
+      <p style={{ marginTop: "115px" }}> hello</p>
+    </div>
+  );
+};
+
+export default Home;
